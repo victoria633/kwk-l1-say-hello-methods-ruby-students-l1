bdb02ba6ab8a7d828c6448c5720151940eedff73
victoria633 <victoria6411@hotmail.com>
2018-06-05 17:36:48 +0000

    automatically backed up by learn

---
+++ greeting.rb
@@ -0,0 +1,11 @@
+def greeting(local_greeting, your_name, my_name,time_of_day)
+  
+ puts "#{local_greeting} #{your_name}, I am #{my_name}, how's your #{time_of_day}?" 
+  
+end
+
+greeting("hey", "esmy" , "victoria", "morning")
+
+greeting("hey" , "john" , "tori" , "afteroon")
+
+greeting("hey" , "Maya" , "victoria" ,"evening")
---
+++ wake_up.rb
@@ -0,0 +1,15 @@
+def wake_up
+puts "hear alarm and say uuuhhhhh"
+15.times do
+puts "get up wash my face"
+end
+puts "brush my teeth"
+
+puts "put in my contact"
+
+puts "change into some different clothes"
+
+puts "do my hair "
+end
+
+wake_up
